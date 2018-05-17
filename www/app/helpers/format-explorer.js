9968e49c4468670a50321f3d71f3f713ca6c0dae
Lucky-Mining <aleksandr.chornuy@gmail.com>
2018-05-17 17:54:06 +0300

    up

--- www/app/helpers/format-explorer.js
+++ www/app/helpers/format-explorer.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export function formatExplorer(params /*, hash*/) {
-  var url = 'https://ubiqscan.io/';
+  var url = 'https://ropsten.etherscan.io/';
   var type = params[0];
   var value = params[1];
   return url + type + '/' + value;
